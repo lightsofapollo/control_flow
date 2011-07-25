b3fc21686c6e2b8117522d162fc50f128ba77958
James Lal <james@lightsofapollo.com>
2011-07-25 00:19:34 -0700

    Completed base library is now mostly complete. Need to write a feature test

--- lib/control_flow/base.rb
+++ lib/control_flow/base.rb
@@ -1,19 +1,43 @@
 module ControlFlow
   class Base
 
+    class InvalidStatus < Exception
+    end
+
+    class InvalidFlow < Exception
+    end
+
     class_attribute :steps
     class_attribute :flows
 
-    attr_reader :current_flow
+    self.flows = {}
+    self.steps = {}
+
+    attr_reader :current_flow, :context
+
+    def self.inherited(klass)
+      if(self == ControlFlow::Base)
+        klass.steps = self.steps.clone
+        klass.flows = self.flows.clone
+      end
+    end
 
     class << self
 
-      def define_flow(&block)
-
+      # Defines a new flow class for this base
+      #
+      # @param [Symbol] name for the flow
+      # @param [Block] class defintion for flow
+      def define_flow(name, &block)
+        self.flows[name] = Class.new(Flow, &block)
       end
 
-      def define_step(&block)
-
+      # Defines a new step for this base
+      #
+      # @param [Symbol] name for the step
+      # @param [Block] class defintion for step
+      def define_step(name, &block)
+        self.steps[name] = Class.new(Step, &block)
       end
 
     end
@@ -23,22 +47,43 @@ module ControlFlow
     #
     # @param [Object] context in which blocks are evaluated
     def initialize(context)
-
+      @context = context
     end
 
     # Enters a flow (by name)
     #
     # @param [Symbol] name of flow
     def enter_flow(flow)
-
+      if(flows.has_key?(flow))
+        @current_flow = flows[flow].new(context, steps)
+      else
+        raise(InvalidFlow, "invalid flow given use: #{flows.keys.join(', ')}")
+      end
     end
 
     # Enters state the flow must be entered 
     # before the state can be activated
-    def enter_state
-
+    def enter_step(state)
+      if(current_flow)
+        current_flow.enter_step(state)
+      else
+        raise(InvalidStatus, 'must enter a state before setting step')
+      end
     end
 
+    # Returns true when flow or self has method
+    def respond_to?(method, *args)
+      current_flow.respond_to?(method) || super
+    end
+
+    # Calls method on flow if available
+    def method_missing(method, *args)
+      if(current_flow.respond_to?(method))
+        current_flow.send(method, *args)
+      else
+        super
+      end
+    end
 
   end
 end
--- spec/lib/control_flow/base_spec.rb
+++ spec/lib/control_flow/base_spec.rb
@@ -2,4 +2,205 @@ require 'spec_helper'
 
 describe ControlFlow::Base do
 
+  let(:klass) do
+    Class.new(ControlFlow::Base) do
+
+      define_flow :free do
+        add_step :one
+      end
+      
+      define_step :one do
+
+      end
+
+    end
+  end
+
+  let(:object) do
+    klass.new(context)
+  end
+
+  let(:context_klass) do
+    Class.new do
+
+      attr_reader :called
+
+      def initialize
+        @called = []
+      end
+
+      def amazing_method
+        true
+      end
+
+      def return_name
+        'zomg'
+      end
+
+    end
+  end
+
+  let(:context) do
+    context_klass.new
+  end
+
+  describe "#self.inherited" do
+
+    context "when inheriting from base" do
+
+      specify { ControlFlow::Base.steps.object_id.should_not == klass.steps.object_id }
+      specify { ControlFlow::Base.flows.object_id.should_not == klass.flows.object_id }
+
+    end
+
+  end
+
+  describe "#self.define_flow" do
+
+    before do
+      klass.define_flow :paid do
+        add_step(:one)
+      end
+    end
+
+    it "should have created new flow class stored in flows[:paid]" do
+      inherited_from = (ControlFlow::Flow > klass.flows[:paid])
+      inherited_from.should === true
+    end
+
+    it "should have executed block in class creation context" do
+      klass.flows[:paid].step_list.should == [:one]
+    end
+
+  end
+
+  describe "#self.define_step" do
+
+    before do
+      klass.define_step :login do
+        value(:one)
+      end
+    end
+
+    it "should have created new step class stored in flows[:paid]" do
+      inherited_from = (ControlFlow::Step > klass.steps[:login])
+      inherited_from.should === true
+    end
+
+    it "should have executed block in class creation context" do
+      klass.steps[:login]._value.should == :one
+    end
+
+  end
+
+  context "#initialize" do
+
+    it "should have setup context in object" do
+      object.context.should == context
+    end
+
+  end
+
+  context "#enter_flow" do
+
+    context "when flow is valid" do
+      before do
+        klass.define_step :one do
+
+        end
+        object.enter_flow(:free)
+      end
+
+      specify { klass.steps[:one].should_not be_nil }
+
+      it "should have initialized flow as current_flow" do
+        object.current_flow.should be_an(ControlFlow::Flow)
+        object.current_flow.context.should == context
+      end
+
+      it "should have set current steps in flow" do
+        object.current_flow.steps.keys.should == object.steps.keys
+      end
+
+    end
+
+    context "when flow is invalid" do
+      before do
+        klass.define_step :one do
+
+        end
+      end
+
+      it "should raise invalid flow exception" do
+        lambda { object.enter_flow(:fake) }.should raise_exception(
+          ControlFlow::Base::InvalidFlow,
+          "invalid flow given use: #{object.flows.keys.join(', ')}"
+        )
+      end
+
+    end
+
+  end
+
+  context "#enter_step" do
+
+    context "when flow is set" do
+
+      before do
+        object.enter_flow(:free)
+        object.enter_step(:one)
+      end
+
+      it "should have entered state :one in free flow" do
+        expected = object.current_flow.steps[:one]
+        object.current_flow.current_step.should == expected
+      end
+
+    end
+
+    context "when no flow is given" do
+
+      it "should raise exception" do
+        lambda { object.enter_step(:one) }.should raise_exception(
+          ControlFlow::Base::InvalidStatus,
+          'must enter a state before setting step'
+        )
+      end
+
+    end
+
+  end
+
+  describe "#respond_to?" do
+
+    before do
+      object.enter_flow(:free)
+      object.enter_step(:one)
+
+      # flexmock(object.current_flow).should_receive(:valid?).once
+    end
+
+    it "should return true when given valid?" do
+      object.should respond_to(:valid?)
+    end
+
+    it "should return false when given invalid method" do
+      object.should_not respond_to(:wowzomg)
+    end
+
+  end
+  
+  describe "#method_missing" do
+
+    before do
+      object.enter_flow(:free)
+      object.enter_step(:one)
+    end
+
+    it "should call valid? on flow when called on base" do
+      object.should be_valid
+    end
+
+  end
+
 end
